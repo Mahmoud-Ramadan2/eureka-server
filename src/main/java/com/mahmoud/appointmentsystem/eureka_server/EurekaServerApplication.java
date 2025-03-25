9917e39f8ad31505eeb145208c7143467ceab37d
Mahmoud-Ramadan2 <mahmoudramadan385@gmail.com>
2025-03-25 14:05:00 +0200

    Config eureka-serve

--- src/main/java/com/mahmoud/appointmentsystem/eureka_server/EurekaServerApplication.java
+++ src/main/java/com/mahmoud/appointmentsystem/eureka_server/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.mahmoud.appointment.eureka;
+package com.mahmoud.appointmentsystem.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/mahmoud/appointmentsystem/eureka_server/EurekaServerApplicationTests.java
+++ src/test/java/com/mahmoud/appointmentsystem/eureka_server/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.mahmoud.appointment.eureka;
+package com.mahmoud.appointmentsystem.eureka_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
